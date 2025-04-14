fabb2543bf4b1d86fbe5622b76f6819637a5dfe1
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-04-13 21:40:08 -0400

    Try useState with an object

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-import Button from './components/Button/Button';
 
 function App() {
-  console.warn('Component mounted (ngOnInit equivalent)');
-
-  useEffect(() => {
-    console.log('Component mounted (ngOnInit equivalent)');
-  }, []);
-
-  const [isVisible, setVisibility] = useState(false);
-  let counter = 0;
-
-  const handleClick = () => {
-    setVisibility(!isVisible);
-    console.log(isVisible);
-  };
+  const [person, setPerson] = useState({
+    firstName: 'Gilles',
+    lastName: 'Ferrand',
+  });
 
   return (
     <>
-      <Button onButtonClicked={handleClick}>Test</Button>
-      {isVisible && <div>isVisible: {isVisible.toString()}</div>}
-      <div>Counter: {counter}</div>
+      <p>
+        {person.firstName} {person.lastName}
+      </p>
     </>
   );
 }
