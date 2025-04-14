aeb790bd12e0ab2ee30f46964b88126ef59e2bb9
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-04-13 21:48:24 -0400

    Update object without mutation

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [person, setPerson] = useState({
-    firstName: 'Gilles',
-    lastName: 'Ferrand',
+  const [drink, setDrink] = useState({
+    title: 'Canadiano',
+    price: 5,
   });
+
+  const handleClick = () => {
+    setDrink({
+      ...drink,
+      price: drink.price + 1,
+    });
+  };
 
   return (
     <>
       <p>
-        {person.firstName} {person.lastName}
+        {drink.title} {drink.price}
       </p>
+      <button onClick={handleClick}>Add 1</button>
     </>
   );
 }
