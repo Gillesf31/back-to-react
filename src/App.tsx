d93b40dc3a429af5e9fe38965f996981a934f7e2
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-04-14 21:37:16 -0400

    Try immer to update objects without spread operator

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
+import produce from 'immer';
 import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: 'Canadiano',
-    price: 5,
-  });
+  const [bugs, setBugs] = useState([
+    { id: 1, title: 'Bug 1', description: 'Description 1', fixed: false },
+    { id: 2, title: 'Bug 2', description: 'Description 2', fixed: false },
+    { id: 3, title: 'Bug 3', description: 'Description 3', fixed: false },
+  ]);
 
   const handleClick = () => {
-    setDrink({
-      ...drink,
-      price: drink.price + 1,
-    });
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce(bugs, (draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
     <>
-      <p>
-        {drink.title} {drink.price}
-      </p>
+      <pre>{JSON.stringify(bugs, null, 2)}</pre>
       <button onClick={handleClick}>Add 1</button>
     </>
   );
