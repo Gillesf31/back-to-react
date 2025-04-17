a4f30cfe92b9d9237a847eb44a738b5df8b6e0db
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-04-17 18:24:30 -0400

    Use useState hook to handle values

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,23 +1,17 @@
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const [person, setPerson] = useState<
-    { name: string; age: number } | undefined
-  >(undefined);
+  const [person, setPerson] = useState({
+    name: '',
+    age: '',
+  });
+
+  const effect = useEffect(() => {
+    console.log(person);
+  }, [person]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // nameRef.current && console.log(nameRef.current.value);
-    // ageRef.current && console.log(ageRef.current.value);
-    nameRef.current &&
-      ageRef.current &&
-      setPerson({
-        name: nameRef.current.value,
-        age: parseInt(ageRef.current.value),
-      });
-
     console.log(person);
   };
 
@@ -27,13 +21,29 @@ const Form = () => {
         <label htmlFor='name' className='form-label'>
           Name
         </label>
-        <input ref={nameRef} type='text' className='form-control' id='name' />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          type='text'
+          className='form-control'
+          id='name'
+        />
       </div>
       <div className='mb-3'>
         <label htmlFor='age' className='form-label'>
           Age
         </label>
-        <input ref={ageRef} type='number' className='form-control' id='age' />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          value={person.age}
+          type='number'
+          className='form-control'
+          id='age'
+        />
       </div>
       <button type='submit' className='btn btn-primary'>
         Submit
