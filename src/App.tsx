e9d40a63608230c324ffe5c9bf7536591d2a7f59
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-04-17 18:06:36 -0400

    Try Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import './App.css';
-import ExpandableText from './components/ExpandableText/ExpandableText';
+import Form from './components/Form/Form';
 
 function App() {
   return (
     <>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, similique
-        dolor! Totam, earum iusto. At perspiciatis magnam est libero aut, itaque
-        fugiat distinctio, nostrum, ducimus totam eaque quasi iure
-        exercitationem eos voluptas ex? Earum dolorum praesentium aliquam cumque
-        vero officia excepturi, eius eligendi alias doloribus consequatur nulla
-        rerum labore. Culpa consectetur, tempora repudiandae repellat quaerat
-        explicabo impedit eius. Nobis ad odio fugiat beatae eveniet vitae
-        molestias placeat aliquam, iusto quasi inventore amet eos quos quam.
-      </ExpandableText>
+      <Form />
     </>
   );
 }
