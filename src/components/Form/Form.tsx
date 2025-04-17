f1cef81ba0b460dd490bfad2323de4abb89e75aa
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-04-17 18:17:09 -0400

    Use useRef hook to get form values

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,24 @@
+import { useRef, useState } from 'react';
+
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const [person, setPerson] = useState<
+    { name: string; age: number } | undefined
+  >(undefined);
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    // nameRef.current && console.log(nameRef.current.value);
+    // ageRef.current && console.log(ageRef.current.value);
+    nameRef.current &&
+      ageRef.current &&
+      setPerson({
+        name: nameRef.current.value,
+        age: parseInt(ageRef.current.value),
+      });
+
+    console.log(person);
   };
 
   return (
@@ -9,13 +27,13 @@ const Form = () => {
         <label htmlFor='name' className='form-label'>
           Name
         </label>
-        <input type='text' className='form-control' id='name' />
+        <input ref={nameRef} type='text' className='form-control' id='name' />
       </div>
       <div className='mb-3'>
         <label htmlFor='age' className='form-label'>
           Age
         </label>
-        <input type='number' className='form-control' id='age' />
+        <input ref={ageRef} type='number' className='form-control' id='age' />
       </div>
       <button type='submit' className='btn btn-primary'>
         Submit
