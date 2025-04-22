b51e01a26e6ab564d5667d6d6f90d7e620ab0cbd
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-04-22 07:51:44 -0400

    Add a loading spinner, works with strict mode too

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,52 @@ type User = {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    let isMounted = true;
 
     (async () => {
       try {
+        if (isMounted) {
+          setIsLoading(true);
+        }
+
+        console.warn('try');
         const res = await axios.get<User[]>(
           'https://jsonplaceholder.typicode.com/users',
           {
             signal: controller.signal,
           }
         );
-        setUsers(res.data);
+
+        if (isMounted) {
+          setUsers(res.data);
+        }
       } catch (err: any) {
         if (err instanceof CanceledError) return;
-        setError(err.message);
+
+        if (isMounted) {
+          setError(err.message);
+        }
+      } finally {
+        console.warn('finally');
+        if (isMounted) {
+          setIsLoading(false);
+        }
       }
     })();
 
-    return () => controller.abort();
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
   }, []);
 
   return (
     <>
+      {isLoading && <div className='spinner-border'></div>}
       <ul>
         {users.map((user: User) => (
           <li key={user.id}>{user.name}</li>
