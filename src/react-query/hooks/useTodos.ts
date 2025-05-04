6524904020d72996b209524c8f7445bfb62db92c
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-05-04 07:34:15 -0400

    Extract hook to ensure SOC

---
+++ src/react-query/hooks/useTodos.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+type Todo = {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+};
+
+export const fetchTodos = async (): Promise<Todo[]> => {
+  const response = await axios.get<Todo[]>(
+    'https://jsonplaceholder.typicode.com/todos'
+  );
+  return response.data;
+};
+
+const useTodos = () => {
+  return useQuery<Todo[], Error>({
+    queryKey: ['todos'],
+    queryFn: fetchTodos,
+  });
+};
+
+export default useTodos;
