911d6232f868da19ed1ca1c266bb0edbf3ce53a2
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-05-04 20:45:29 -0400

    Try different fetch options

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  // defaultOptions: {
+  //   queries: {
+  //     retry: 3,
+  //     cacheTime: 300_000,
+  //     staleTime: 10_000,
+  //     refetchOnWindowFocus: false,
+  //     refetchOnReconnect: false,
+  //     refetchOnMount: false,
+  //   },
+  // },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
