d456a5d9998983b55df502569fae90ac8a12c615
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-06-28 10:13:15 -0400

    Add nested routes

---
+++ src/routing/Navbar.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav
+      className='navbar navbar-expand-lg'
+      style={{ background: '#f0f0f0', marginBottom: '1rem' }}
+    >
+      <div className='container-fluid'>
+        <Link to='/' className='navbar-brand'>
+          My App
+        </Link>
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <Link to='/' className='nav-link'>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/users' className='nav-link'>
+                Users
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
